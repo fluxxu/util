709da32d0caa035cf8cf764d3b14fa791b9dc43c
Flux Xu <fluxxu@gmail.com>
2015-04-02 18:15:35 -0400

    Add Provider.

---
+++ provider.go
@@ -0,0 +1,140 @@
+package util
+
+import (
+	"net/http"
+	"regexp"
+	"strconv"
+)
+
+const MaxNumOfFilterValue int = 5
+const MaxTake uint64 = 30
+
+type Filters map[string][]string
+
+type Pager interface {
+	SetTake(v uint)
+	SetSkip(v uint)
+}
+
+type Filter interface {
+	LimitFilterKeys(keys ...string)
+	AddFilter(key, value string)
+	GetFilters() Filters
+}
+
+type Sorter interface {
+	LimitSortKeys(keys ...string)
+	SetSort(key string)
+	GetSort() string
+}
+
+type Provider interface {
+	Pager
+	Filter
+	Sorter
+	ParseRequest(r *http.Request)
+
+	Count() (int, error)
+	Read(dst interface{}) error
+}
+
+type BaseProvider struct {
+	take       uint64
+	skip       uint64
+	filterKeys map[string]bool
+	sortKeys   map[string]bool
+
+	filters Filters
+	sort    string
+}
+
+func (p *BaseProvider) SetTake(v uint64) {
+	if v > MaxTake {
+		v = MaxTake
+	}
+	p.take = v
+}
+
+func (p *BaseProvider) SetSkip(v uint64) {
+	p.skip = v
+}
+
+func (p *BaseProvider) AddFilter(k, v string) {
+	if _, ok := p.filterKeys[k]; ok {
+		s := p.filters[k]
+		if len(s) < MaxNumOfFilterValue {
+			for _, item := range s {
+				if item == v {
+					return
+				}
+			}
+			p.filters[k] = append(s, v)
+		}
+	}
+}
+
+func (p *BaseProvider) GetFilters() Filters {
+	return p.filters
+}
+
+func (p *BaseProvider) SetSort(k string) {
+	if _, ok := p.sortKeys[k]; ok {
+		p.sort = k
+	}
+}
+
+func (p *BaseProvider) GetSort() string {
+	return p.sort
+}
+
+func (p *BaseProvider) LimitFilterKeys(keys ...string) {
+	for _, v := range keys {
+		p.filterKeys[v] = true
+	}
+}
+
+func (p *BaseProvider) LimitSortKeys(keys ...string) {
+	for _, v := range keys {
+		p.sortKeys[v] = true
+	}
+}
+
+var filterR = regexp.MustCompile(`^filter_([\w]+)`)
+
+func (p *BaseProvider) ParseRequest(r *http.Request) {
+	params := r.URL.Query()
+	for k, values := range params {
+		if len(values) == 0 {
+			continue
+		}
+
+		lv := values[len(values)-1]
+
+		if k == "sort" {
+			p.SetSort(lv)
+		} else if k == "take" {
+			if iv, err := strconv.Atoi(lv); err == nil {
+				p.SetTake(uint64(iv))
+			}
+		} else if k == "skip" {
+			if iv, err := strconv.Atoi(lv); err == nil {
+				p.SetSkip(uint64(iv))
+			}
+		} else if m := filterR.FindSubmatch([]byte(k)); m != nil {
+			fk := string(m[1])
+			for _, v := range values {
+				p.AddFilter(fk, v)
+			}
+		}
+	}
+}
+
+func NewBaseProvider() *BaseProvider {
+	p := &BaseProvider{
+		filterKeys: make(map[string]bool),
+		sortKeys:   make(map[string]bool),
+		filters:    make(Filters),
+		take:       MaxTake,
+	}
+	return p
+}
---
+++ provider_sql.go
@@ -0,0 +1,103 @@
+package util
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/lann/squirrel"
+	"reflect"
+	"strings"
+)
+
+type SqlProvider struct {
+	*BaseProvider
+	table  string
+	dbx    *sqlx.DB
+	fields []string
+}
+
+func (p *SqlProvider) ParseStruct(s interface{}) {
+	t := reflect.TypeOf(s)
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		dbtag := strings.TrimSpace(f.Tag.Get("db"))
+		if dbtag != "" && dbtag != "-" {
+			p.fields = append(p.fields, dbtag)
+			//sort, filter
+			tag := strings.TrimSpace(f.Tag.Get("provider"))
+			for _, attr := range strings.Split(tag, " ") {
+				attr = strings.TrimSpace(attr)
+				if attr == "sort" {
+					p.sortKeys[dbtag] = true
+				} else if attr == "filter" {
+					p.filterKeys[dbtag] = true
+				}
+			}
+		}
+	}
+}
+
+type wherePart struct {
+	pred interface{}
+	args []interface{}
+}
+
+func (p *SqlProvider) filterWhereParts() []wherePart {
+	var parts []wherePart
+	for k, vs := range p.GetFilters() {
+		var part wherePart
+		var preds []string
+		for _, v := range vs {
+			preds = append(preds, fmt.Sprintf("(`%s` LIKE ?)", k))
+			part.args = append(part.args, "%"+strings.Replace(v, "%", "", -1)+"%")
+		}
+		part.pred = "(" + strings.Join(preds, " OR ") + ")"
+		parts = append(parts, part)
+	}
+	return parts
+}
+
+func (p *SqlProvider) Count() (int, error) {
+	var count int
+	q := squirrel.Select("COUNT(*)").From(p.table)
+	for _, part := range p.filterWhereParts() {
+		q = q.Where(part.pred, part.args...)
+	}
+	sql, args, err := q.ToSql()
+	if err != nil {
+		return 0, err
+	}
+	//fmt.Println("Count:" + sql)
+	if err = p.dbx.Get(&count, sql, args...); err != nil {
+		return 0, err
+	}
+	return count, nil
+}
+
+func (p *SqlProvider) Read(dst interface{}) error {
+	q := squirrel.Select(p.fields...).From(p.table).Limit(p.take).Offset(p.skip)
+	for _, part := range p.filterWhereParts() {
+		q = q.Where(part.pred, part.args...)
+	}
+	sql, args, err := q.ToSql()
+	if err != nil {
+		return err
+	}
+	//fmt.Println("Select:" + sql)
+	if err = p.dbx.Select(dst, sql, args...); err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewSqlProvider(dbx *sqlx.DB, table string, s interface{}) *SqlProvider {
+	p := &SqlProvider{
+		table: table,
+		dbx:   dbx,
+	}
+	p.BaseProvider = NewBaseProvider()
+	p.ParseStruct(s)
+	//fmt.Println("db:", p.fields)
+	//fmt.Println("filter:", p.filterKeys)
+	//fmt.Println("sort:", p.sortKeys)
+	return p
+}
